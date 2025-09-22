c4f3408075e8d01f54cf9936c8a356b38b92862f
yuz010.kim <rladbwmd123@gmail.com>
2025-09-22 10:34:51 +0900

    remove dat var and index

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,8 +1,6 @@
 user_attendance_data = {}
 id_cnt = 0
 
-# dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
 points = [0] * 100
 grade = [0] * 100
 names = [''] * 100
@@ -20,34 +18,22 @@ def input2(user_name, weekday):
     id2 = user_attendance_data[user_name]
 
     add_point = 0
-    index = 0
+    WEEKEND = ["saturday", "sunday"]
 
+    if weekday in WEEKEND:
+        add_point += 2
+        weeken[id2] += 1
     if weekday == "monday":
-        index = 0
         add_point += 1
     elif weekday == "tuesday":
-        index = 1
         add_point += 1
     elif weekday == "wednesday":
-        index = 2
         add_point += 3
         wed[id2] += 1
     elif weekday == "thursday":
-        index = 3
         add_point += 1
     elif weekday == "friday":
-        index = 4
         add_point += 1
-    elif weekday == "saturday":
-        index = 5
-        add_point += 2
-        weeken[id2] += 1
-    elif weekday == "sunday":
-        index = 6
-        add_point += 2
-        weeken[id2] += 1
-
-    dat[id2][index] += 1
     points[id2] += add_point
 
 
@@ -59,17 +45,18 @@ def read_raw_text(file_name:str):
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
 
-def input_file(file_name: str):
+def run_attendance_check(file_name:str):
     user_attendance_raw_data = read_raw_text(file_name)
     for user_data in user_attendance_raw_data:
         if len(user_data) != 2:
             continue
-        input2(user_data[0], user_data[1])
+        [user_name, weekday] = user_data
+        input2(user_name, weekday)
 
     for i in range(1, id_cnt + 1):
-        if dat[i][2] > 9:
+        if wed[i] > 9:
             points[i] += 10
-        if dat[i][5] + dat[i][6] > 9:
+        if weeken[i] > 9:
             points[i] += 10
 
         if points[i] >= 50:
@@ -95,4 +82,4 @@ def input_file(file_name: str):
 
 
 if __name__ == "__main__":
-    input_file("attendance_weekday_500.txt")
+    run_attendance_check("attendance_weekday_500.txt")
